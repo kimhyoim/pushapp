94fa3e998dfa384646a2c4b294e055638f9e8cde
kimhyoim <kimhyoim23@gmail.com>
2018-12-30 17:40:21 +0900

    푸시 앱 테스트

---
+++ app/src/main/java/kr/sp/pushapp/MainActivity.java
@@ -0,0 +1,26 @@
+package kr.sp.pushapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button getTokenButton = (Button) findViewById(R.id.checkTokenButton);
+        getTokenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyFirebaseInstanceIDService service = new MyFirebaseInstanceIDService();
+                service.onTokenRefresh();
+            }
+        });
+    }
+}
